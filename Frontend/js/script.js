ef4e0b0f88b955b4167a839ce3d8531a09910a33
[Bartek Michorczyk] <[bartlomiejmichorczyk@gmail.com]>
2023-12-02 20:39:44 +0100

    Zpuszowalem :D

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,73 +1,66 @@
 const app = Vue.createApp({
-    data: function () {
+  data: function () {
       return {
-        title: 'NutriMade',
-        nutri_info_1: 'Dieta Wegańska',
-        nutri_info_2: 'Dieta Wegetariańska',
-        nutri_info_3: 'Bez Laktozy',
-        nutri_info_4: 'Cukrzyca',
-        nutri_info_5: 'Kalorie',
-        text: '',
-        loading: false,
-        vegan_diet: false,
-        vegetarian_diet: false,
-        no_lactose: false,
-        Diabetes: true,
-        Calories: '',
-        Meal_type: '',
-        Preferences: '',
+          title: 'NutriMade',
+          nutri_info_1: 'Dieta Wegańska',
+          nutri_info_2: 'Dieta Wegetariańska',
+          nutri_info_3: 'Bez Laktozy',
+          nutri_info_4: 'Cukrzyca',
+          nutri_info_5: 'Kalorie',
+          text: '',
+          loading: false,
+          vegan_diet: false,
+          vegetarian_diet: false,
+          no_lactose: false,
+          Diabetes: false,
+          Calories: '',
+          Meal_type: '',
+          Preferences: '',
       }
-    },
-    methods: {
+  },
+  methods: {
       async sendNutriForm() {
-          // Data to be sent in the request body (if any)
-          // Data to be sent in the request body (if any)
           const requestData = {
-            vegan_diet: this.vegan_diet,
-            vegetarian_diet: this.vegetarian_diet,
-            no_lactose: this.no_lactose,
-            Diabetes: this.Diabetes,
-            Calories: this.Calories,
-            Meal_type: this.Meal_type,
-            Preferences: this.Preferences,
-        };
+              vegan_diet: this.vegan_diet,
+              vegetarian_diet: this.vegetarian_diet,
+              no_lactose: this.no_lactose,
+              Diabetes: this.Diabetes,
+              Calories: this.Calories,
+              Meal_type: this.Meal_type,
+              Preferences: this.Preferences,
+          };
 
           this.loading = true;
 
-          // method post
           const response = await fetch(`http://127.0.0.1:8000/generate-dish-propositions`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'accept': 'application/json',
-              // You may need to include additional headers such as authentication tokens
-            },
-            body: JSON.stringify(requestData), // Convert the data to JSON format
-            }
-            );
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'accept': 'application/json',
+              },
+              body: JSON.stringify(requestData),
+          });
 
-            this.loading = false;
+          const data = await response.json();
+          this.text = data;
 
-            setTimeout(() =>  {
-              window.location.href = 'propozycje.html'
-            }, 500)
-
-            // ;
+          this.loading = false;
       },
       getData() {
-        return this.text
+          return this.text
       },
-      // Method to handle toggle clicks
       handleToggleClick(property) {
-        this[property] = !this[property];
-        console.log(this[property]);
-      },
-    }, 
-    computed: {
-      // Computed property to dynamically select the toggle image based on the data property
-      toggleImage() {
-          return this.vegan_diet ? 'assets/img/toggleOn.svg' : 'assets/img/toggleOff.svg';
+          this[property] = !this[property];
+          console.log(this[property]);
       },
   },
-  }); 
-  
+  computed: {
+      toggleImage() {
+          return function (property) {
+              return this[property] ? 'assets/img/toggleOn.svg' : 'assets/img/toggleOff.svg';
+          };
+      },
+  },
+});
+
+const mountedApp = app.mount('#app');

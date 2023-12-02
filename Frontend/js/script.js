d40342b36f944b36f5f9f697bd08818f052cb88c
mateuszrucinski <m.rucinski15@gmail.com>
2023-12-02 13:29:30 +0100

    add sendNutriForm endpoint in js

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -10,19 +10,40 @@ const app = Vue.createApp({
       }
     },
     methods: {
-      async getFacts() {
-          const response = await fetch(`http://127.0.0.1:8000/catfact/${this.number}`);
+      async sendNutriForm() {
+          const response = await fetch(`http://127.0.0.1:8000/generate-dish-propositions`);
+          // Data to be sent in the request body (if any)
+          const requestData = {
+            vegan_diet: true,
+            vegetarian_diet: true,
+            no_lactose: true,
+            Diabetes: true,
+            Calories: 0,
+            Meal_type: "string",
+            Preferences: "string"
+          };
+
+          // Configure the request
+          const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json',
+            // You may need to include additional headers such as authentication tokens
+          },
+          body: JSON.stringify(requestData), // Convert the data to JSON format
+          };
           
-          if (response.ok) {
-            const data = await response.json();
-            this.text = data;
+          // if (response.ok) {
+          //   const data = await response.json();
+          //   this.text = data;
             
-            // Data was sent successfully, navigate to another page
-            window.location.href = 'frontNext.html';
-          } else {
-            // Handle error cases if needed
-            console.error('Backend request failed');
-          }
+          //   // Data was sent successfully, navigate to another page
+          //   window.location.href = 'frontNext.html';
+          // } else {
+          //   // Handle error cases if needed
+          //   console.error('Backend request failed');
+          // }
       },
       getData() {
         return this.text
